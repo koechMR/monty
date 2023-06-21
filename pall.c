78dc86d7ec1d3bec1dfca5af44be9bb8ed7eb810
koechMR <briankipkoech328@gmail.com>
2023-06-21 21:25:15 +0300

    new test

--- pall.c
+++ pall.c
@@ -1,21 +1,53 @@
 #include "monty.h"
-/**
- * f_pall - fucntion that prints the stack
- * @head: head of the stack
- * @counter: no used
- * Return: returns nothing
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
 
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
+/**
+ * push - Pushes an element to the stack
+ * @stack: Pointer to the stack
+ * @value: Value to be pushed
+ * @line_number: Line number in the file
+ */
+void push(stack_t *stack, int value, unsigned int line_number)
+{
+	if (stack->top == MAX_STACK_SIZE - 1)
 	{
-		printf("%d\n", h->a);
-		h = h->next;
+		fprintf(stderr, "L%u: Stack overflow\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	stack->data[++stack->top] = value;
+}
+
+/**
+ * pall - Prints all values on the stack
+ * @stack: Pointer to the stack
+ */
+void pall(stack_t *stack)
+{
+	int i;
+
+	for (i = stack->top; i >= 0; i--)
+	{
+		printf("%d\n", stack->data[i]);
 	}
 }
+
+/**
+ * main - Entry point of the program
+ * Return: 0 on success
+ */
+int main(void)
+{
+	stack_t stack;
+
+	stack.top = -1;
+
+
+	push(&stack, 1, 1);
+	push(&stack, 2, 2);
+	push(&stack, 3, 3);
+
+	pall(&stack);
+
+	return (0);
+}
+
