ca2cb1a2045d4bc96e3987a4508c43ea0b8b4990
koechMR <briankipkoech328@gmail.com>
2023-06-21 20:03:17 +0300

    new

--- push.c
+++ push.c
@@ -1,53 +1,37 @@
 #include "monty.h"
-
 /**
- * push - Pushes an element to the stack
- * @stack: Pointer to the stack
- * @value: Value to be pushed
- * @line_number: Line number in the file
- */
-void push(stack_t *stack, int value, unsigned int line_number)
+ * f_push - add nodes to the stack of that is being shown
+ * @head: head of the stack
+ * @counter: where the line counts
+ * Return: return nothing
+*/
+void f_push(stack_t **head, unsigned int counter)
 {
-	if (stack->top == MAX_STACK_SIZE - 1)
+	int v, a = 0, flag = 0;
+
+	if (bus.arg)
 	{
-		fprintf(stderr, "L%u: Stack overflow\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	stack->data[++stack->top] = value;
+		if (bus.arg[0] == '-')
+			a++;
+		for (; bus.arg[a] != '\0'; a++)
+		{
+			if (bus.arg[a] > 57 || bus.arg[a] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	v = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, v);
+	else
+		addqueue(head, v);
 }
-
-/**
- * pall - Prints all values on the stack
- * @stack: Pointer to the stack
- */
-void pall(stack_t *stack)
-{
-	int i;
-
-	for (i = stack->top; i >= 0; i--)
-	{
-		printf("%d\n", stack->data[i]);
-	}
-}
-
-/**
- * main - Entry point of the program
- * Return: 0 on success
- */
-int main(void)
-{
-	stack_t stack;
-
-	stack.top = -1;
-
-
-	push(&stack, 1, 1);
-	push(&stack, 2, 2);
-	push(&stack, 3, 3);
-
-	pall(&stack);
-
-	return (0);
-}
-
